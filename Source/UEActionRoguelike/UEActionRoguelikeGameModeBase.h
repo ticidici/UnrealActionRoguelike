45df0c0ff816041c53c84cf471540a637675c1aa
Dani <ticidici@yahoo.es>
2023-09-21 21:16:08 +0200

    unreal project created

---
+++ Source/UEActionRoguelike/UEActionRoguelikeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UEActionRoguelikeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UEACTIONROGUELIKE_API AUEActionRoguelikeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
